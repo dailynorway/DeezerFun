20cf0f628178d8cd13bde46cbe7f0bd5443beeac
Erick Vavretchek <erickva@gmail.com>
2020-02-10 15:16:39 +0100

    Implement Tests for Deezer API client for Artists

---
+++ DeezerFunTests/DeezerApiClientArtistsTests.swift
@@ -0,0 +1,117 @@
+//
+//  DeezerApiClientArtistTests.swift
+//  DeezerFunTests
+//
+//  Created by Erick Vavretchek on 10/2/20.
+//  Copyright © 2020 Erick Vavretchek. All rights reserved.
+//
+
+import XCTest
+@testable import DeezerFun
+
+class DeezerApiClientArtistTests: XCTestCase {
+
+    var testBundle: Bundle!
+    fileprivate var httpClient: HttpClientMock!
+    var client: DeezerApiClient!
+    
+    override func setUp() {
+        testBundle = Bundle(for: type(of: self))
+        httpClient = HttpClientMock(testBundle: testBundle)
+        client = DeezerApiClient(httpClient: httpClient)
+    }
+
+    func testSearchArtist() {
+        httpClient.errorSimulationType = .noError
+        client.searchArtist("Kygo") { result in
+            if case let Result.success(artists) = result {
+                XCTAssert(artists[1].name == "Kygo & Justin Jesso", "Name of first artist return must be Kygo")
+                return
+            }
+            XCTFail()
+        }
+    }
+    
+    func testSearchArtistWithDataNotReceivedError() {
+        httpClient.errorSimulationType = .dataNotReceived
+        client.searchArtist("Kygo") { result in
+            if case let Result.failure(error) = result {
+                XCTAssert(error.localizedDescription == "API did not return data", "We must receive a Data Not received error")
+                return
+            }
+            XCTFail()
+        }
+    }
+    
+    func testSearchArtistWithUrlSessionError() {
+        httpClient.errorSimulationType = .urlSessionError
+        client.searchArtist("Kygo") { result in
+            if case let Result.failure(error) = result {
+                XCTAssert(error.localizedDescription == "The operation couldn’t be completed. (NSURLErrorDomain error -1000.)", "We must receive a Data Not received error")
+                return
+            }
+            XCTFail()
+        }
+    }
+    
+    func testSearchArtistWithDecodingError() {
+        httpClient.errorSimulationType = .decodingError
+        client.searchArtist("Kygo") { result in
+            if case let Result.failure(error) = result {
+                XCTAssert(error.localizedDescription == "The data couldn’t be read because it is missing.", "We must receive Decoding missing error")
+                return
+            }
+            XCTFail()
+        }
+    }
+    
+    func testSearchArtistWithDeezerNativeError() {
+        httpClient.errorSimulationType = .deezerNativeError
+        client.searchArtist("Kygo") { result in
+            if case let Result.failure(error) = result {
+                XCTAssert(error.localizedDescription == "This is a Deezer Native Error", "We must receive a Deezer Native Error")
+                return
+            }
+            XCTFail()
+        }
+    }
+}
+
+fileprivate class HttpClientMock: HttpClient {
+    let testBundle: Bundle
+    init(testBundle: Bundle) { self.testBundle = testBundle }
+    
+    enum ErrorSimulationType {
+        case dataNotReceived
+        case urlSessionError
+        case decodingError
+        case deezerNativeError
+        case noError
+    }
+    var errorSimulationType: ErrorSimulationType!
+    
+    override func sendRequest(router: DeezerApiClient.Router, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        
+        switch errorSimulationType {
+        case .dataNotReceived:
+            completionHandler(nil, nil, nil)
+        case .urlSessionError:
+            completionHandler(nil, nil, URLError.init(.badURL))
+        default:
+            let data: Data
+            switch router {
+            case .searchArtist:
+                if errorSimulationType == .decodingError {
+                    data = testBundle.dataFromFile("ArtistsBadlyFormatted.json")
+                } else if errorSimulationType == .deezerNativeError {
+                    data = testBundle.dataFromFile("DeezerNativeError.json")
+                } else {
+                    data = testBundle.dataFromFile("Artists.json")
+                }
+                completionHandler(data, nil, nil)
+            default:
+                break
+            }
+        }
+    }
+}
