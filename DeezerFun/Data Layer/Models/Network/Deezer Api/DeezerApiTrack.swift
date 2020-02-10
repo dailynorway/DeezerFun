c9af769827da6a7cbf7e9f1d629823262106cd3a
Erick Vavretchek <erickva@gmail.com>
2020-02-10 10:14:31 +0100

    Create Track models

---
+++ DeezerFun/Data Layer/Models/Network/Deezer Api/DeezerApiTrack.swift
@@ -0,0 +1,27 @@
+//
+//  DeezerApiTrack.swift
+//  DeezerFun
+//
+//  Created by Erick Vavretchek on 10/2/20.
+//  Copyright © 2020 Erick Vavretchek. All rights reserved.
+//
+
+import Foundation
+
+struct DeezerApiTrack: Codable {
+    let id: Int
+    let title: String
+    let shortTitle: String
+    let position: Int
+    let duration: Int
+    let preview: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case shortTitle = "title_short"
+        case position = "track_position"
+        case duration
+        case preview
+    }
+}
---
+++ DeezerFun/Data Layer/Models/Track.swift
@@ -0,0 +1,18 @@
+//
+//  Track.swift
+//  DeezerFun
+//
+//  Created by Erick Vavretchek on 10/2/20.
+//  Copyright © 2020 Erick Vavretchek. All rights reserved.
+//
+
+import Foundation
+
+struct Track {
+    let id: Int
+    let title: String
+    let shortTitle: String
+    let position: Int
+    let duration: Int
+    let preview: URL
+}
