a65aed5588545905255964bc9656d920b8acd4da
Erick Vavretchek <erickva@gmail.com>
2020-02-11 14:14:14 +0100

    Create files for Tracks module

---
+++ DeezerFun/Modules/Tracks/TrackCell.swift
@@ -0,0 +1,19 @@
+//
+//  TrackCell.swift
+//  DeezerFun
+//
+//  Created by Erick Vavretchek on 11/2/20.
+//  Copyright © 2020 Erick Vavretchek. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+    
+    static let reuseIdentifier = "TrackCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
---
+++ DeezerFun/Modules/Tracks/TracksDataSource.swift
@@ -0,0 +1,9 @@
+//
+//  TracksDataSource.swift
+//  DeezerFun
+//
+//  Created by Erick Vavretchek on 11/2/20.
+//  Copyright © 2020 Erick Vavretchek. All rights reserved.
+//
+
+import Foundation
